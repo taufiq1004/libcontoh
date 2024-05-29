30b134aac60442d04580d92cd981a63a139133bb
taufiq1004 <taufiqurrahman101010@gmail.com>
2024-05-29 09:22:17 +0700

    commit pertama

---
+++ src/helloworld.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace taufiq\libhello;
+
+class helloworld{
+    public function sayhello ($name){
+        return "hello $name  welcome to php and composer your own library";
+    }
+}
